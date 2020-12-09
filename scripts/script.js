83de82c77a183812042e1300eceb024f2f8d70d9
Raihan <sirradf@gmail.com>
2020-12-09 12:39:09 +0700

    Add auto dark mode > 12 pm

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
 const mode = document.querySelector('.mode');
+const body = document.querySelector('body');
+const us = document.querySelector('.upper-section');
+
+setDark();
 
 mode.addEventListener(`click`, () => {
     modeToggle();
 });
 
 function modeToggle(){
-    const body = document.querySelector('body');
-    const us = document.querySelector('.upper-section');
     us.classList.toggle('dark');
     body.classList.toggle('dark');
     mode.classList.toggle('darkmd-btn');
+}
+
+function setDark(){
+    const date = new Date();
+    let dateHours = date.getHours();
+    if(date > 12){
+        us.classList.add('dark')
+        body.classList.add('dark');
+    }
+    console.log(dateHours)
 }
